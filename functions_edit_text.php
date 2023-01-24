84edd3b52ec8d3e3326067bb41b48f96c385753b
Xaobin <40346917+Xaobin@users.noreply.github.com>
2023-01-24 15:29:20 -0400

    Many functions Php - Edit Files

---
+++ functions_edit_text.php
@@ -0,0 +1,360 @@
+<?php
+$stra="lkjkvjsdlkv
+
+ccasdvkasdnv
+
+dsvadvkanskd
+
+Lana Del Rey@ @ @ h t tps://www.youtube.com/watch?v=z-6cCmxaGoQ@ hi de @
+
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#divSUbPy3' role='button' aria-expanded='false'>Show/Hide</a>
+Lana Del Rey - <a href='https://www.youtube.com/watch?v=z-6cCmxaGoQ' target='_blank'>FILE_LINK</a>
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#divAqvVfZ' role='button' aria-expanded='false'>Show/Hide</a>
+Mais tretas - <a href='https://www.youtube.com/watch?v=op34sv05RjI' target='_blank'>FILE_LINK</a>
+Universidade do Paraná - Links - <a href='https://www.inf.ufpr.br/andrey/ci062/' target='_blank'>_LINK_</a>
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#div6ktE2f' role='button' aria-expanded='false'>Show/Hide</a>
+Modelagem OO - Fatores de refatoração - <a href='https://www.inf.ufpr.br/andrey/ci062/TapAula12.pdf' target='_blank'>FILE_LINK</a>
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#divp1oMAt' role='button' aria-expanded='false'>Show/Hide</a>
+Intro ao Haskell - <a href='https://www.inf.ufpr.br/andrey/ci062/TapAula17.pdf' target='_blank'>FILE_LINK</a>
+";
+
+$strb="
+lkjkvjsdlkv
+
+ccasdvkasdnv
+
+dsvadvkanskd
+
+Lana Del Rey
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#divSr8UoF' role='button' aria-expanded='false'>Show/Hide</a><a href='https://www.youtube.com/watch?v=z-6cCmxaGoQ' target='_blank'>FILE_LINK</a>
+
+Olá 
+<a href='http://algo.com' target='_blank'>_LINK_</a>";
+
+$strc=" 
+Something Something Something Something Something 
+Something Something Something Something Something 
+
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#divJWbaRo' role='button' aria-expanded='false'>Show/Hide</a>&nbsp;  &nbsp;   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; 5. Mon Dieu [Gdu23_zdYgE].m4a - <a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_4af99de16deaaaa6567b.m4a' target='_blank'>FILE_LINK</a><a class='btn btn-outline-secondary' data-toggle='collapse' href='#divuVOuNR' role='button' aria-expanded='false'>Show/Hide</a>&nbsp;  &nbsp;   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; 6. Maladie d&#8217;un Fou [gKld49qtCas].m4a - <a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_d09b92b40f4b072badcf.m4a' target='_blank'>FILE_LINK</a><a class='btn btn-outline-secondary' data-toggle='collapse' href='#div10LvCP' role='button' aria-expanded='false'>Show/Hide</a>&nbsp;  &nbsp;   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; 7. AC [MsAAAi4OlUw].m4a - <a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_3c45fa4e71caea8aa9cf.m4a' target='_blank'>FILE_LINK</a><a class='btn btn-outline-secondary' data-toggle='collapse' href='#div8YUIZN' role='button' aria-expanded='false'>Show/Hide</a>&nbsp;  &nbsp;   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; 8. Noir Désir [fKDYuI1F8NQ].m4a - <a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_ea61f55c628983607e63.m4a' target='_blank'>FILE_LINK</a><a class='btn btn-outline-secondary' data-toggle='collapse' href='#divAUgtq7' role='button' aria-expanded='false'>Show/Hide</a>&nbsp;  &nbsp;   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp; 9. KL [eIXQfb6fzuU].m4a - <a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_b5a97ee14d307834c7b6.m4a' target='_blank'>FILE_LINK</a>
+";
+
+$strd="Something Something Something Something Something 
+Something Something Something Something Something 
+
+Something @@@<a href='https://www.youtube.com/watch?v=z-6cCmxaGoQ@hide@' target='_blank'>https://www.youtube.com/watch?v=z-6cCmxaGoQ@hide@</a>
+
+<a href='http://algo.com' target='_blank'>http://algo.com</a>
+
+Something @@@<a href='https://www.inf.ufpr.br/andrey/ci062/@hide@' target='_blank'>https://www.inf.ufpr.br/andrey/ci062/@hide@</a>
+
+Something @@@<a href='https://www.inf.ufpr.br/andrey/ci062/TapAula12.pdf@hide@' target='_blank'>https://www.inf.ufpr.br/andrey/ci062/TapAula12.pdf@hide@</a>
+
+<a class='btn btn-outline-secondary' data-toggle='collapse' href='#divSUbPy3' role='button' aria-expanded='false'>Show/Hide</a>Lana Del Rey - <a href='https://www.youtube.com/watch?v=z-6cCmxaGoQ' target='_blank'>FILE_LINK</a>
+
+Ça va je t&#8217;aime (Remastered) [XJEf0a5CIuw].mp3 - <a href='http://localhost/ska/public/assets/uploads/folder1/1673707500_cc664f5375196745b0a5.mp3' target='_blank'>FILE_LINK</a>
+";
+$stre="Something Something Something Something Something
+Something Something Something Something Something
+
+___betag___a class='btn btn-outline-secondary' data-toggle='collapse' href='#divJWbaRo' role='button' aria-expanded='false'___entag___Show/Hide___betag___/a___entag___              5. Mon Dieu [Gdu23_zdYgE].m4a - ___betag___a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_4af99de16deaaaa6567b.m4a' target='_blank'___entag___FILE_LINK___betag___/a___entag______betag___a class='btn btn-outline-secondary' data-toggle='collapse' href='#divuVOuNR' role='button' aria-expanded='false'___entag___Show/Hide___betag___/a___entag___              6. Maladie d’un Fou [gKld49qtCas].m4a - ___betag___a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_d09b92b40f4b072badcf.m4a' target='_blank'___entag___FILE_LINK___betag___/a___entag______betag___a class='btn btn-outline-secondary' data-toggle='collapse' href='#div10LvCP' role='button' aria-expanded='false'___entag___Show/Hide___betag___/a___entag___              7. AC [MsAAAi4OlUw].m4a - ___betag___a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_3c45fa4e71caea8aa9cf.m4a' target='_blank'___entag___FILE_LINK___betag___/a___entag______betag___a class='btn btn-outline-secondary' data-toggle='collapse' href='#div8YUIZN' role='button' aria-expanded='false'___entag___Show/Hide___betag___/a___entag___              8. Noir Désir [fKDYuI1F8NQ].m4a - ___betag___a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_ea61f55c628983607e63.m4a' target='_blank'___entag___FILE_LINK___betag___/a___entag______betag___a class='btn btn-outline-secondary' data-toggle='collapse' href='#divAUgtq7' role='button' aria-expanded='false'___entag___Show/Hide___betag___/a___entag___              9. KL [eIXQfb6fzuU].m4a - ___betag___a href='https://hagadel.xyz/ska/public/assets/uploads/folder1/1673621671_b5a97ee14d307834c7b6.m4a' target='_blank'___entag___FILE_LINK___betag___/a___entag___";
+//================================
+function makeArray($str){
+    $entag="___entag___";
+    $betag="___betag___";
+    $neoarr=array(); $cont=0;
+    $fullstr="";
+    $aux=array();
+    $arr=array();
+    $arr=explode(PHP_EOL,$str);
+     $cc=count($arr); $cc--; 
+   
+    for ($i=0;$i<=$cc;$i++){
+        
+        if (strlen($arr[$i])>=400){
+            $arr[$i]=str_replace($entag.$betag,$entag."\n".$betag,$arr[$i]);
+           
+            $fullstr.=$arr[$i];
+        } else{
+        
+        $fullstr.=$arr[$i]."\n";
+        }
+    }
+    $neoarr=explode(PHP_EOL,$fullstr);
+    return $neoarr;
+}
+//================================
+//================================
+function printSimpleArr($arr){
+  $cc=count($arr); $cc--; 
+   echo "<br>Print Simple Array begin<br>";
+    for ($i=0;$i<=$cc;$i++){
+        echo $arr[$i]."";
+        echo "<br>---------------------------<br>";
+    }
+    echo "<br>End Print Simple Array <br>";
+}
+//================================
+function printArr($strd){
+
+$betag="___betag___"; $entag="___entag___";
+    $strd=str_replace("<",$betag,$strd);
+    $strd=str_replace(">",$entag,$strd);
+//$arr=explode(PHP_EOL,$strd);
+echo "<br>Before array treatment:<br>".nl2br($strd)."<br><br>";
+$arr=makeArray($strd);
+//printSimpleArr($arr);
+ $cc=count($arr); $cc--; 
+  // /*
+    for ($i=0;$i<=$cc;$i++){
+    if (isset($arr[$i])){
+        $href=getHref($arr[$i]); $atext=getAText($arr[$i]);
+        $initxt=getBeginText($arr[$i]); $realtxt=getRealText($arr[$i],$href);
+        $collapse=getCollapse($arr[$i]);
+        echo "Array I.....:".$arr[$i]."<br>";
+        echo "Get HRef....:".$href."<br>";
+        echo "Get AText...:".$atext."<br>"; 
+        echo "Get IniText.:".$initxt."<br>"; 
+        echo "Get RealTxt.:".$realtxt."<br>"; 
+        echo "Get Collapse:".$collapse."<br>";
+        echo "Get Combo 1:".$initxt.$realtxt.$collapse."<br>"; 
+        echo "Get Combo 2:".$initxt.$realtxt.$href."<br>"; 
+        echo "<br>--------------------------------------<br>";
+     }   
+    }
+    //*/
+}
+//=================================
+function refact_tags($strd){
+$betag="___betag___"; $entag="___entag___";
+    $strd=str_replace("<",$betag,$strd);
+    $strd=str_replace(">",$entag,$strd);
+//$arr=explode(PHP_EOL,$strd);
+$arr=makeArray($strd);
+ $cc=count($arr); $cc--; 
+   $neostring="";
+    for ($i=0;$i<=$cc;$i++){
+     if (isset($arr[$i])){
+       if ($arr[$i]!=null){
+        $href=getHref($arr[$i]); $atext=getAText($arr[$i]);
+        $initxt=getBeginText($arr[$i]); $realtxt=getRealText($arr[$i],$href);
+        if ($initxt==$realtxt){ $initxt=""; }
+        $collapse=getCollapse($arr[$i]);
+       if ($collapse!=null){
+        $neostring.=$initxt.$realtxt.$collapse;
+       } else{ $neostring.=$initxt.$realtxt.$href; }
+       
+        $neostring.="\n"; 
+      } else{ $neostring.="\n";  }
+     }
+    }
+    $neostring=str_replace("&nbsp;  &nbsp;   &nbsp;  &nbsp;  &nbsp;  &nbsp;  &nbsp;","",$neostring);
+    return $neostring;
+}
+//=================================
+function getHref($str){
+$lnk="";
+$timo=explode("a href='",$str);
+ if (isset($timo[1])){
+    $cc=strlen($timo[1]); $cc--;
+    for ($i=0;$i<=$cc;$i++){
+        if ($timo[1][$i]=="'"){ break; }
+        $lnk.=$timo[1][$i];
+    }
+  }
+  return $lnk;
+
+}
+//=================================
+function getAText($str){
+    $betag="___betag___"; $entag="___entag___";
+    $str=str_replace($betag,"<",$str);
+    $str=str_replace($entag,">",$str);
+    $str=strip_tags($str);
+    return $str;
+    
+}
+//=================================
+function getBeginText($str){
+    $nstr=""; $cc=0;
+    $betag="___betag___"; $entag="___entag___";
+    $str=str_replace($betag,"<",$str);
+    $str=str_replace($entag,">",$str);
+    $cc=strlen($str); $cc--;
+    for ($i=0;$i<=$cc;$i++){
+        if ($str[$i]!="<"){
+            $nstr.=$str[$i];
+        }
+        if ($str[$i]=="<"){ break; }
+    }
+    return $nstr;
+}
+//=================================
+function getRealText($str,$link){
+   $notext=getAText($str);
+   $notext=str_replace("\n","",$notext);
+   
+  
+   
+   
+   
+   $notext=str_replace("FILE_LINK","",$notext);
+    $notext=str_replace("Show/Hide","",$notext);
+    $notext=str_replace("http","",$notext);
+    $link=str_replace("http","",$link);
+    $link=str_replace("@@@","",$link);
+      $link=str_replace("@hide@","",$link);
+      $notext=str_replace("@@@","",$notext);
+       $link=str_replace("@hide@","",$link);
+  
+   $tt=strlen($notext);
+   if ($tt>=8){
+   $stripPart1=$notext[$tt-8].$notext[$tt-7].$notext[$tt-6].$notext[$tt-5].$notext[$tt-4].$notext[$tt-3].$notext[$tt-2].$notext[$tt-1];
+      
+      
+   //$stripPart1=substr($notext,7);
+      //echo "<br>StripPart1:".$stripPart1.":..EndStringpart1<br>";
+  // echo "<br>Link......:".$link.":..Endlink<br>"; 
+  // echo "<br>Notext......:".$notext.":...EndNotext<br>"; 
+   
+    if ($stripPart1!=null){
+      if  (substr_count($link,$stripPart1)==0) 
+         return $notext;
+    }
+    }
+    return "";
+    
+}
+//=================================
+function getCollapse($str){
+    $ret="";
+    if (substr_count($str,"collapse")>0){
+        $ret="@@@".getHref($str)."@hide@";
+    }
+    return $ret;
+}
+//=================================
+function alter_strip_links($strx){
+if (substr_count($strx,"href")>0){
+    $betag="___betag___"; $entag="___entag___";
+    $strx=str_replace("<",$betag,$strx);
+    $strx=str_replace(">",$entag,$strx);
+    //echo ">Vec aft be.en.tags><br>".$strx."<br>";
+   // $arr=explode($betag."/a".$entag,$strx);
+   $arr=explode(PHP_EOL,$strx);
+   printArr($arr);
+/*
+    $cc=count($arr); $cc--; $metemp="";
+    $mestring=""; $next=false;
+    for ($i=0;$i<=$cc;$i++){
+        $metemp=$arr[$i].$betag."/a".$entag;
+        $metemp=getTreatHref($metemp);
+        if ($next==true){
+            $metemp=str_replace("\n","",$metemp);
+            $metemp=$metemp."@hide@";
+            $next=false;
+        } else {
+            if ($metemp=="@hide@"){ $next=true; }
+        }
+        $metemp=str_replace("@hide@http","http",$metemp);
+        $mestring.=$metemp;
+
+    }
+    //$mestring=str_replace("__hide__","@hide@",$mestring);
+    //$mestring=removeBlHide($mestring);
+    $mestring=nl2br($mestring);
+    $mestring=str_replace("\n","_nl_",$mestring);
+    $mestring=str_replace("<br />","_br_",$mestring);
+    $mestring=str_replace("_br_","",$mestring);
+    $mestring=str_replace("@hide@http","http",$mestring);
+    $mestring=str_replace("_nl__nl__nl__nl_@hide@","@hide@\n",$mestring);
+    $mestring=str_replace("_nl__nl__nl_@hide@","@hide@\n",$mestring);
+    $mestring=str_replace("_nl__nl_@hide@","@hide@\n",$mestring);
+    $mestring=str_replace("_nl_@hide@","@hide@",$mestring);
+    
+    $mestring=str_replace("_nl_","\n",$mestring);
+    $mestring=setAtSign($mestring);
+    // 
+    
+    
+    return $mestring;
+  } else{ return $strx; }
+  */
+}}
+//===================================
+function getTreatHref($strx){
+    $betag="___betag___"; $entag="___entag___";
+    $timo=explode("a href='",$strx); $strip="";
+    $timo[0]=str_replace($betag,"<",$timo[0]);
+    $timo[0]=str_replace($entag,">",$timo[0]);
+    $timo[0]=strip_tags($timo[0]);
+    //$timo[0]=str_replace("\n\r","",$timo[0]);
+
+    $coll="";
+    if (substr_count($strx,"collapse")>=1){
+        $timo[0]=str_replace("FILE_LINK","..",$timo[0]);
+        $timo[0]=str_replace("Show/Hide","",$timo[0]);
+        $timo[0]=str_replace("http","@@@http",$timo[0]);
+        return $timo[0]."@hide@";
+    }
+    $nstr="";
+    if (isset($timo[1])){
+    $cc=strlen($timo[1]); $cc--;
+    for ($i=0;$i<=$cc;$i++){
+        if ($timo[1][$i]=="'"){ break; }
+        $nstr.=$timo[1][$i];
+    }
+    }
+    //$nstr.=$coll; $nstr=trim($nstr);
+    $strx=str_replace($betag,"<",$strx);
+    $strx=str_replace($entag,">",$strx);
+
+
+
+    $strip=strip_tags($strx);
+    $strip=str_replace("FILE_LINK","",$strip);
+    $strip=str_replace("Show/Hide","",$strip);
+    $stripPart1=substr($nstr,7);
+   
+   
+    if ($stripPart1!=null){
+      if  (substr_count($stripPart1,$strip)==0) 
+         { return $nstr."\n"; }
+    }
+    return $strip.$nstr;
+}
+//===================================
+function setAtSign($strx){
+    $arr=explode(PHP_EOL,$strx);
+    $cc=count($arr); $cc--; $grouper="";
+    for ($i=0;$i<=$cc;$i++){
+        if (substr_count($arr[$i],"@hide@")>0){
+            $grouper.="@@@".$arr[$i]."\n";
+        }else{ $grouper.=$arr[$i]."\n"; }
+    }
+    return $grouper;
+}
+//===================================
+//echo ">>Result>><br>".getTreatHref($strb);
+$actual=$strc;
+echo "......Str original......<br>".nl2br($actual);
+echo "<br><br><br>";
+echo "<br>==========================================================<br>";
+$binn="nArcanum";
+
+//if ($binn=="Arcanum"){
+    printArr($actual);
+    echo "<br>==========================================================<br>";
+//}
+//else{
+   $binn=refact_tags($actual);
+   echo nl2br($binn);
+//}
+echo "<br>";
+
+?>
+<html>
+ <body>
+    <textarea rows="20" cols="140"><? echo $binn; ?>
+    </textarea>
+ </body>
+</html>
